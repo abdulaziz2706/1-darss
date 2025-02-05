001f09ee8df2e58f0cbc44a1f3f20e808c145a92
abdulaziz2706 <abdulaziz27707@gmail.com>
2025-02-05 15:54:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let ism = prompt('Ismingizni kiriting');
+
+let yoshi = prompt('yoshingizni kiriting')
+
+let familiyangiz = prompt('familiyangizni kiriting');
+
+let maktabingiz = prompt('maktabingizni kiriying');
+
+let sinifingiz = prompt ('sinifingizni kiriting')
+
+console.log(ism);
+
+console.log('sizning yoshingiz =@#$% ', yoshi)
+
+console.log('familiyangiz')
+
+console.log('maktabingiz')
+
+console.log(' sinifingiz')
+
+
+
+
+
+
+
+
+
+
+
+
